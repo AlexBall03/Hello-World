078226108d55fc05fd827f12126b68aa52636696
AlexBall03 <alexanderdball03@gmail.com>
2022-04-23 09:32:37 -0700

    Edited Main.c

--- Main.c
+++ Main.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 
 int main() {
-    char helloWorld[] = "Hello World!";
+    char helloWorld[] = "Hello World! \n";
     
     if (helloWorld[0] == 'H' && helloWorld[1] == 'e') {
-        printf("%s", helloWorld);
+        for (int i = 0; i < 5; i++) {
+            printf(helloWorld);
+        }
     }
+
+   
 
     return 0;
 }
